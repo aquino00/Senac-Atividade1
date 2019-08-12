cc66b0470985a9bd10b4df58cb2d65418995bf25
aquino00 <sudokobo@gmail.com>
2019-08-12 00:17:28 -0300

    adicionando o player

---
+++ TiroAnimacao/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField]
+    private float walkspeed;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(walkspeed, 0);
+    }
+
+}
