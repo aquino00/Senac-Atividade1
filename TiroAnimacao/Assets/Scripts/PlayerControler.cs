b0aae100dd4e146b7f76231e0e46b4fbec335f0c
aquino00 <sudokobo@gmail.com>
2019-08-12 20:02:33 -0300

    andar e pular

--- TiroAnimacao/Assets/Scripts/PlayerControler.cs
+++ TiroAnimacao/Assets/Scripts/PlayerControler.cs
@@ -10,6 +10,12 @@ public class PlayerControler : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    private Vector2 newMovement;
+
+    private bool facingRight = true;
+
+    private bool jump;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -27,7 +33,27 @@ public class PlayerControler : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(walkspeed, 0);
+        rb.velocity = newMovement;
     }
+    public void Move(float direction)
+    {
+        float currentSpeed = walkspeed;
+        newMovement = new Vector2(direction * currentSpeed, rb.velocity.y);
+
+        if(facingRight && direction < 0)
+        {
+            Flip();
+        }
+        else if(!facingRight && direction > 0)
+        {
+            Flip();
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
 
 }
---
+++ TiroAnimacao/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public PlayerControler playerControler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerControler.Move(Input.GetAxisRaw("Horizontal"));
+    }
+}
